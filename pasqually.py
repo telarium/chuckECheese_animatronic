95291066e880d96e910bebd871bfee2698a5cab3
telarium <andrew@langleycreations.com>
2016-03-02 02:28:27 +0000

    More IO functionality

--- pasqually.py
+++ pasqually.py
@@ -19,17 +19,19 @@ from pygame.locals import *
 
 Setup()
 
+gpio = GPIO()
+
 airCompressorOffHourStart = 1 # The hour of the day to switch off the air compressor. Change to None to disable.
 airCompressorOffHourEnd = 7 # The hour of the day to turn the air compressor back on after a restful evening. Change to None to disable.
 rebootHour = 2 # The hour of the day to reboot CHIP to reset everything. Change to None to disable.
 def sendWebKey( key, val ):
 	key = key.lower()
-        movements.executeMovement( key, val )
+	print val
+        movements.executeMovement( key, val, gpio.set )
 
 pygame.init()
 isRunning = True
 clock = pygame.time.Clock()
-gpio = GPIO()
 movements = Movement(gpio)
 webServer = WebServer(sendWebKey)
 
