5478ddf8bd0d7b5332a8c2b9396861b02e066220
telarium <andrew@langleycreations.com>
2024-10-18 16:41:02 -0700

    More horrible gamepad code

--- start.py
+++ start.py
@@ -9,42 +9,50 @@ from animatronic_movements import Movement
 from gamepad_input import USBGamepadReader
 
 class Pasqually:
-    def __init__(self):
-        self.gpio = GPIO()
-        self.movements = Movement(self.gpio)
-        self.webServer = WebServer()
-        self.systemInfo = SystemInfo(self.webServer)
-        self.gamepad = USBGamepadReader()
-        self.setDispatchEvents()
-        self.isRunning = True
+	def __init__(self):
+		self.gpio = GPIO()
+		self.movements = Movement(self.gpio)
+		self.webServer = WebServer()
+		self.systemInfo = SystemInfo(self.webServer)
+		self.gamepad = USBGamepadReader()
+		self.setDispatchEvents()
+		self.isRunning = True
 
-    def setDispatchEvents(self):
-        dispatcher.connect(self.onKeyEvent, signal="keyEvent", sender=dispatcher.Any)
-        dispatcher.connect(self.onConnectEvent, signal="connectEvent", sender=dispatcher.Any)
+	def setDispatchEvents(self):
+		dispatcher.connect(self.onKeyEvent, signal='keyEvent', sender=dispatcher.Any)
+		dispatcher.connect(self.onGamepadKeyEvent, signal='gamepadKeyEvent', sender=dispatcher.Any)
+		dispatcher.connect(self.onConnectEvent, signal='connectEvent', sender=dispatcher.Any)
 
-    def onSystemInfoEvent(self, cpu, ram):
-        print(cpu)
+	def onSystemInfoEvent(self, cpu, ram):
+		print(cpu)
 
-    def onConnectEvent(self):
-        self.webServer.broadcast('movementInfo', self.movements.getKeyboardKeys())
+	def onConnectEvent(self):
+		self.webServer.broadcast('movementInfo', self.movements.getKeyboardKeys())
 
-    def onKeyEvent(self, key, val):
-        try:
-            self.movements.executeMovement(str(key).lower(), val)
-        except Exception as e:
-            print(f"Invalid key: {e}")
+	def onKeyEvent(self, key, val):
+		try:
+			self.movements.executeMovement(str(key).lower(), val)
+		except Exception as e:
+			print(f"Invalid key: {e}")
 
-    def run(self):
-        try:
-            while self.isRunning:
-                time.sleep(0.01)
-        except KeyboardInterrupt:
-            self.shutdown()
+	def onGamepadKeyEvent(self, key, val):
+		try:
+			if self.movements.executeMovement(str(key).lower(), val):
+				self.webServer.broadcast('gamepadKeyEvent', str(key).lower(), val)
+		except Exception as e:
+			print(f"Invalid key: {e}")
 
-    def shutdown(self):
-        if self.webServer:
-            self.webServer.shutdown()
+	def run(self):
+		try:
+			while self.isRunning:
+				time.sleep(0.01)
+		except KeyboardInterrupt:
+			self.shutdown()
+
+	def shutdown(self):
+		if self.webServer:
+			self.webServer.shutdown()
 
 if __name__ == "__main__":
-    animatronic = Pasqually()
-    animatronic.run()
+	animatronic = Pasqually()
+	animatronic.run()
